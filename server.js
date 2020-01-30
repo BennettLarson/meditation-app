b5d796d1a7c2281a6270ca97bd8398d7e71e1a86
Bennett Larson <bennett.larson@objectpartners.com>
2020-01-29 22:29:54 -0600

    graphql setup

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const app = express();
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '/build')));
+// // Serve the static files from the React app
+// app.use(express.static(path.join(__dirname, '/build')));
 
-app.get('/api', (req, res) => {
-	res.send('An alligator approaches!');
-});
+// app.get('/api', (req, res) => {
+// 	res.send('An alligator approaches!');
+// });
 
-// Handles any requests that don't match the ones above
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/build/index.html'));
-});
+// // Handles any requests that don't match the ones above
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.join(__dirname + '/build/index.html'));
+// });
 
-app.listen(9000, () => console.log('Gator app listening on port 9000!'));
+// app.listen(9000, () => console.log('Gator app listening on port 9000!'));
+
+
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema.js')
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+	schema: schema,
+	graphiql: true
+}))
+
+app.listen(3000, () => {
+	console.log('App listening on port 3000')
+})
